c41499c8a8c3194411d9fe591352df5fec6b0117
Adam Harbas <adam.harbas@socialexplorer.com>
2025-08-28 09:48:28 +0200

    Add new dag

---
+++ dags/example_dag_2.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="example_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    task1 = BashOperator(
+        task_id="print_date",
+        bash_command="date"
+    )
+
+    task2 = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello from Astro!'"
+    )
+
+    task1 >> task2
